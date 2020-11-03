0808e606cfb7af76c4ace1819b2e371431c9c610
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2020-11-03 12:28:50 +0800

    Pseudocode Q2

---
+++ PseudoCodeToJavaQ2.java
@@ -0,0 +1,16 @@
+public class PseudoCodeToJavaQ2 {
+    public static void main(String[] args) {
+        int x = 2;
+        int c = 1;
+        while (c <= 50){
+            if (x % 4 == 0){
+                x = x + 2;
+                continue;
+            }
+            System.out.println(x);
+            x = x + 2;
+            c = c + 1;
+        }
+        System.out.println(c);
+    }
+}
