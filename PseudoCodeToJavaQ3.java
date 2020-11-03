12b8aa38572617b2b0cbeeaf01b7b24861660f18
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2020-11-03 13:50:40 +0800

    PseudoCode Q3

---
+++ PseudoCodeToJavaQ3.java
@@ -0,0 +1,18 @@
+public class PseudoCodeToJavaQ3 {
+    public static void main(String[] args) {
+        float C = 18;
+        float F = 19;
+        int x = 1;
+        int Choice = 1;
+        do{
+            if (Choice == 1){
+                F = (C * (9/5)) + 32;
+                System.out.println("Temperature in Farenheit: " + F);
+            } else if (Choice == 2){
+                C = (F - 32) * (5/9);
+                System.out.println("Temperature in Celcius: " + C);
+            }  
+            x = x + 1;
+        } while (x <= 10);
+    }
+}
