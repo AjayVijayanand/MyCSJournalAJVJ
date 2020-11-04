8b6c34135e282c4f5c364690b5d30fd1a4b798c5
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2020-11-04 20:30:42 +0800

    FreeLanceCode1

---
+++ FreeLancePseudoCodeToJava1.java
@@ -0,0 +1,10 @@
+public class FreeLancePseudoCodeToJava1 {
+    public static void main(String args[]){
+        int x = 0;
+        int factorial = 1;
+        for(int y = 1; y <= x; y++){
+            factorial = factorial * y;
+        }
+        System.out.println(factorial);
+    }
+}
