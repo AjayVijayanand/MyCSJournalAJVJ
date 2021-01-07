9530979f5e433904de79f32a2921b71c0fd4e908
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-01-07 19:23:14 +0800

    User Input

--- FreeLanceCode/StudentInfo(FreeLanceJavaClassNObjectCode)/MainCode.java
+++ FreeLanceCode/StudentInfo(FreeLanceJavaClassNObjectCode)/MainCode.java
@@ -1,14 +1,27 @@
+import java.util.*;
 public class MainCode{
     public static void main(String[] args){
-        int x = 3;
-        StudentBio[] Students = new StudentBio[x];
-        Students[0] = new StudentBio("AJ", 17, 9, 1, 2004);
-        Students[1] = new StudentBio("VJ", 11, 9, 11, 2004);
-        Students[2] = new StudentBio("DJ", 9, 10, 12, 2004);
-        for(StudentBio e : Students){
-            System.out.println(e.getName());
-            System.out.println(e.getAge());
-            System.out.println(e.getDOB());
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Number of Students:");
+        int Range = input.nextInt();
+        StudentBio[] Students = new StudentBio[Range];
+        for(int x = 0; x < Range; x++){
+            System.out.println("Enter Name of Student:");
+            String Name = input.nextLine();
+            System.out.println("Enter Age of Student:");
+            int Age = input.nextInt();
+            System.out.println("Enter Month of Student's Birth:");
+            int DOB = input.nextInt();
+            System.out.println("Enter Day of Student's Birth:");
+            int MOB = input.nextInt();
+            System.out.println("Enter Year of Student's Birth:");
+            int YOB = input.nextInt();
+            Students[x] = new StudentBio(Name, Age, DOB, MOB, YOB);
+        }
+        for(StudentBio f : Students){
+            System.out.println("Name: " + f.getName());
+            System.out.println("Age: " + f.getAge());
+            System.out.println("Date of Birth: " + f.getDOB());
         }
     }
 }
