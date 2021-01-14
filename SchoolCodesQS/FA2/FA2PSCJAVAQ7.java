e78d0789e5b0a970f2f34b51ad21be808f90ad74
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-01-14 09:15:27 +0800

    Folder Rename

--- SchoolCodesQS/FA2/FA2PSCJAVAQ7.java
+++ SchoolCodesQS/FA2/FA2PSCJAVAQ7.java
@@ -12,27 +12,27 @@ public class FA2PSCJAVAQ7 {
         int num3 = input.nextInt();
         if(num1 > num2){
             if(num1 > num3){
-                System.out.println("The largest is: " + num1);
+                System.out.println("The largest is: " + num1 + ". This is Number 1.");
             } else if (num1 == num3){
-                System.out.println("The largest is: " + num1 + " and " + num3 + ". Both num1 and num3 are Equal!");
+                System.out.println("The largest is: " + num1 + ". Both Number 1 and Number 3 are Equal!");
             } else {
-                System.out.println("The largest is: " + num3);
+                System.out.println("The largest is: " + num3 + ". This is Number 3.");
             }
         } else if (num1 < num2){
             if(num2 > num3){
-                System.out.println("The largest is: " + num2);
+                System.out.println("The largest is: " + num2 + ". This is Number 2.");
             } else if (num2 == num3){
-                System.out.println("The largest is: " + num2 + " and " + num3 + ". Both num2 and num3 are Equal!");
+                System.out.println("The largest is: " + num2 + ". Both Number 2 and Number 3 are Equal!");
             } else {
-                System.out.println("The largest is: " + num3);
+                System.out.println("The largest is: " + num3 + ". This is Number 3.");
             }
         } else if (num1 == num2){
             if (num1 == num3){
-                System.out.println("All 3 numbers are equal!");
+                System.out.println("All 3 Numbers are equal!");
             } else if (num1 > num3){
-                System.out.println("The largest is: " + num1 + " and " + num2 + ". Both num1 and num2 are Equal!");
+                System.out.println("The largest is: " + num1 + ". Both Number 1 and Number 2 are Equal!");
             } else {
-                System.out.println("The largest is: " + num3);
+                System.out.println("The largest is: " + num3 + ". This is Number 3.");
             }
         }
     }
