5576775e199bf24fbc01efd5d6a0fcd237638d9d
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-02-08 18:46:17 +0800

    Message

--- Volume7_DimensionalArrays/DAQ15.java
+++ Volume7_DimensionalArrays/DAQ15.java
@@ -5,7 +5,7 @@ public class DAQ15{
         int[] Rev = {3,1,2,6,4,1};
         for (int x = 0; x < Num.length - 1; x++){
             int Index = x;
-            for (int y = x; y < Num.length; y++){
+            for (int y = (x + 1); y < Num.length; y++){
                 if (Rev[Index] > Rev[y]){
                     Index = y;
                 }
--- Volume7_DimensionalArrays/DAQB.java
+++ Volume7_DimensionalArrays/DAQB.java
@@ -1,20 +1,18 @@
 import java.util.*;
-public class DAQ14{
+public class DAQB {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         int[] Num = {3,1,2,6,4,1,6,1,3,4};
         int[] Rev = {3,1,2,6,4,1,6,1,9,4};
-        int Total = 0;
-        int MaximumLength = Num.length - 1;
-        for (int x = 0; x < MaximumLength; x++){
-            for (int y = 0; y < (MaximumLength - x); y++){
+        for (int x = 0; x < 9; x++){
+            for (int y = 0; y < 10; y++){
                 if(Rev[y] > Rev[y+1]){
+                    System.out.println(Arrays.toString(Rev));
                     int temp = Rev[y];
                     Rev[y] = Rev[y+1];
                     Rev[y+1] = temp;
                 }
             }
         }
-        System.out.println("Your Array: " + Arrays.toString(Num));
-        System.out.println("Revised Array: " + Arrays.toString(Rev));
     }
 }
