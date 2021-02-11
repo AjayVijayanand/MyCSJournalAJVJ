3377f3c63100b7bd09e0bd3329d5a449817064a9
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-02-11 14:32:45 +0800

    Create DAQ16.java

---
+++ Volume7_DimensionalArrays/DAQ16.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class DAQ16{
+    public static void main(String[] args){
+        String[] Rev = {"HELLO", "WORLD","MY", "NAME", "IS", "SOMEBODY"};
+        System.out.println("Your Array: " + Arrays.toString(Rev));
+        int MaximumLength = Str.length - 1;
+        for (int x = 0; x < MaximumLength; x++){
+            for (int y = 0; y < (MaximumLength - x); y++){
+                if(Rev[y].compareTo(Rev[y+1]) > 0){
+                    String temp = Rev[y];
+                    Rev[y] = Rev[y+1];
+                    Rev[y+1] = temp;
+                }
+            }
+        }
+        System.out.println("Revised Array: " + Arrays.toString(Rev));
+    }
+}
