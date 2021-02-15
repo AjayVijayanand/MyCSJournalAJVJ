b9b88f3d6e9c1e2eb28f4a135abbc2c3d65e9d15
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-02-15 11:47:22 +0800

    Create GeneticAlgorithm.java

---
+++ VolumeCS_TheCaseStudy/GeneticAlgorithm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class GeneticAlgorithm {
+    public static void main(String[] args){
+        char[] TourC;
+        char[] Tour = {'A', 'B', 'C', 'D'};
+        int[][] TourD = {
+            {0, 21, 34, 12, 11},
+            {21, 0, 54, 67, 45},
+            {34, 54, 0, 15, 56},
+            {12, 67, 15, 0, 19},
+            {11, 45, 56, 19, 0}
+        };
+        while(true){
+            
+        }
+    }
+}
