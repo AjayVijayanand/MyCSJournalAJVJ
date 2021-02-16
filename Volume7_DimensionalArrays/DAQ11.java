958130208305e4be82698c569283286ca71167c9
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-02-16 12:09:37 +0800

    Update DAQ11.java

--- Volume7_DimensionalArrays/DAQ11.java
+++ Volume7_DimensionalArrays/DAQ11.java
@@ -3,18 +3,16 @@ public class DAQ11{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int[] Num = {1, 2, 3, 4, 5, 6, 8, 9};
-        boolean Validation = false;
-        int Index = 0;
-        System.out.print("Enter Number To add: ");
-        int Element = input.nextInt();
         int[] Rev = new int[Num.length + 1];
         int Index = 0;
+        Boolean Added = false;
+        System.out.print("Enter Number To add: ");
+        int Element = input.nextInt();
         for(int x = 0; x <= Num.length - 1; x++){
-            if (Num[Index] < Num[x]){
+            if (Num[Index] < Element){
               Index = x;
             }
         }
-        Boolean Added = false;
         for(int x = 0; x <= Num.length; x++){
             if (!Added){
                 if(x == Index){
