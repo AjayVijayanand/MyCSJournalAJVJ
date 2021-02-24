9e9649a9602f71dcd8cdf475a196d14a392a0fef
DRAJV0956 <45524907+AjayVijayanand@users.noreply.github.com>
2021-02-24 20:01:09 +0800

    Additions

--- Volume7_DimensionalArrays/DAQ11.java
+++ Volume7_DimensionalArrays/DAQ11.java
@@ -2,10 +2,9 @@ import java.util.*;
 public class DAQ11{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int[] Num = {1, 2, 3, 4, 5, 6, 8, 9};
-        int[] Rev = new int[Num.length + 1];
+        int[] Num = {1, 2, 3, 4, 5, 6, 8, 9, 0};
         int Index = 0;
-        Boolean Added = false;
+        System.out.println("Your Array: " + Arrays.toString(Num));
         System.out.print("Enter Number To add: ");
         int Element = input.nextInt();
         for(int x = 0; x <= Num.length - 1; x++){
@@ -13,19 +12,11 @@ public class DAQ11{
               Index = x;
             }
         }
-        for(int x = 0; x <= Num.length; x++){
-            if (!Added){
-                if(x == Index){
-                    Rev[x] = Element;
-                    Added = true;
-                } else {
-                    Rev[x] = Num[x];
-                }
-            } else {
-                Rev[x] = Num[x-1];
-            }
+        int x;
+        for(x = Num.length - 2; x >= Index; x--){
+            Num[x + 1] = Num[x];
         }
+        Num[x + 1] = Element;
         System.out.println("Your Array: " + Arrays.toString(Num));
-        System.out.println("Revised Array: " + Arrays.toString(Rev));
     }
 }
---
+++ Volume8_CT2Practice/CT2PQ1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class CT2PQ1 {
+    public static void main(String[] args){
+        int[] Num = {1, 2, 3, 1, 2, 6, 7, 8, 9};
+        boolean Duplicate = false;
+        for (int x = 0; x < Num.length; x++){
+            for (int y = x+1; y < Num.length; y++){
+                if (Num[x] == Num[y]){
+                    Duplicate = true;
+                    System.out.println("Duplicate Found! It is at positions " + x + " And " + y + ". The Number in both of these positions is: " + Num[x]);
+                }
+            }
+        }
+        if (!Duplicate){
+            System.out.println("Duplicate Not Found!");
+        }
+    }
+}
