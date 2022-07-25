4c79c760532997f5f66809341cb0165687e03290
DRAJV0956 <ajayvijayanand2004@gmail.com>
2022-07-25 22:10:56 +0800

    Wordle

--- VolumeX_FreeLancer/Python/YNotGames/WG.py
+++ VolumeX_FreeLancer/Python/YNotGames/WG.py
@@ -1,33 +1,34 @@
 from random import randint
 
-with open("words.txt", "r") as w:
+with open("VolumeX_FreeLancer\Python\YNotGames\hrt.txt", "r") as w:
     WordList = w.readlines()
 
-while len(Word) != 5:
-    Word = WordList[randint(len(WordList))]
+Word = WordList[randint(0, len(WordList))]
+Word = Word.upper()
 Letters = list(Word)
-RLetters = ['', '', '', '', '']
-GLetters = ['', '', '', '', '']
-Count = 5
+Count = len(Word) - 1
 Tries = 0
-for x in range(Count):
+RLetters = GLetters = [''] * (Count + 1)
+print("This word has " + str(Count) + " Letters.")
+for x in range(Count + 1):
     Tries = Count - x
     Valid = False
     while not Valid:
         try:
             GW = str(input("Enter Guess: "))
-            if len(GW) == 5:
+            GW = GW + " "
+            if len(GW) == Count + 1:
                 Valid = True
             else:
-                print("Enter 5 Letter Word")
+                print("Enter " + str(Count) + " Letter Word")
         except:
             print("Enter Valid Value!")
     GWord = GW.upper()
     GLetters = list(GWord)
     Guessed = True
-    for x in range(len(Word)):
+    for x in range(len(Word) - 1):
         if GLetters[x] == Letters[x]:
-            RLetters[x] = GLetters[x]
+            RLetters[x] = Letters[x]
         else:
             Guessed = False
             RLetters[x] = "*"
@@ -40,7 +41,7 @@ for x in range(Count):
         WronglyPlaced = []
         for y in Letters:
             for z in GLetters:
-                if y == z and y not in RLetters:
+                if y == z and y not in RLetters and y not in WronglyPlaced:
                     WronglyPlaced.append(y)
         print("Some Letters that are wrongly placed: " + str(WronglyPlaced))
         print("Tries Left " + str(Tries))
@@ -48,5 +49,6 @@ for x in range(Count):
 if not Guessed:
     print("You have completed all tries!")
     print("But unfortunately you have not guessed the correct word :(")
-
+if Guessed:
+    print("It took you " + str(x) + " Guesses")
 print("The Correct word is: " + Word)
