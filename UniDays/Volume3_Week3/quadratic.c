c5aa96a0e43aabcc04a651e6bb5d010153c7e7d8
DRAJV0956 <ajayvijayanand2004@gmail.com>
2024-08-28 13:57:54 +0800

    Quadratic

--- UniDays/Volume3_Week3/quadratic.c
+++ UniDays/Volume3_Week3/quadratic.c
@@ -10,21 +10,20 @@ int main(void) {
     printf("Input the coefficient, c: ");
     scanf("%lf", &c);
     d = b*b-4*a*c;
-    /* Compute the roots. */
-    if (a == 0) {                                   /* If coefficient a is zero, do the below. Note the double equal or == sign */
+    if (a == 0) {                                                                      /* Linear Equation */
         printf("There is only 1 root!");
         printf("The Root: %4.3f", -c/b);
     } else {
-        if (d > 0) {
+        if (d > 0) {                                                                   /* 2 Real Distinct Root */
             printf("There are 2 distinct roots!!");
             root1 = (- b + sqrt(d))/(2*a);
             root2 = (- b - sqrt(d))/(2*a);
             printf("First root: %4.3f\n", root1);
             printf("Second root: %4.3f\n", root2);
-        } else if (d == 0) {
+        } else if (d == 0) {                                                            /* 1 Real Root */
             printf("There is only 1 root!\n");
             printf("The root: %4.3f\n", -b/(2*a));
-        } else {
+        } else {                                                                        /* 2 Complex Roots */
             printf("There are Complex Roots!\n");
             realpart = (-b)/(2*a);
             imaginarypart = sqrt(-d)/(2*a);
